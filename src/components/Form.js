800befefa7b7fd0e7fa8ff2fb9da19a93c8d702a
HENtum <bogdan_pet@mail.ru>
2022-08-30 23:41:48 +0300

    1

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,29 +1,26 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export class Form extends Component {
-  App = ()=>{
+export default function Form(props) {
+  function App(){
     return(<div>
-      {this.props.pressure>1013?'Давление повышенное':'Давление пониженное'}
+      {props.pressure>1013?'Давление повышенное':'Давление пониженное'}
     </div>
     )
   }
-  render() {
-  return (
+   return (
       <div>
-        {this.props.cityName&&
+        {props.cityName&&
         <div className='formDiv1'>
-          <p className='formP1'>{'Место: '+this.props.cityName+' , '+ this.props.country}</p>
-          <p>{'Температура: '+Math.floor(this.props.temp-273.15)+ '°C'}</p>
-          <p>{'Ветер: '+Math.floor(this.props.wind)+' м/с'}</p>
-          <p>{'Облака: '+this.props.sky}</p>
-          <p>{'Давление: '+this.props.pressure}</p>
-          <p className='formP2'>{this.App()}</p>
+          <p className='formP1'>{'Место: '+ props.cityName+' , '+ props.country}</p>
+          <p>{'Температура: '+Math.floor(props.temp-273.15)+ '°C'}</p>
+          <p>{'Ветер: '+ Math.floor(props.wind)+' м/с'}</p>
+          <p>{'Облака: '+ props.sky}</p>
+          <p>{'Давление: '+ props.pressure}</p>
+          <p className='formP2'>{App()}</p>
         </div>
         }
-        <p className='formErr1'onMouseEnter={()=>{this.props.errMov()}}onMouseLeave={()=>{this.props.errLeave()}}>{this.props.error}</p>
+        <p className='formErr1'onMouseEnter={()=>{props.errMov()}}onMouseLeave={()=>{props.errLeave()}}>{props.error}</p>
       </div>
     )
-  }
 }
 
-export default Form
