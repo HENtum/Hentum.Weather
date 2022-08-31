818e22a029c0f9e6a7b828b73eb41b13c29208fa
HENtum <bogdan_pet@mail.ru>
2022-08-31 21:09:30 +0300

    1

--- src/components/Weath.js
+++ src/components/Weath.js
@@ -1,29 +1,15 @@
-import React, { Component } from 'react'
+import React,{useState} from 'react'
 
-export class Weath extends Component {
-  constructor(props){
-    super(props);
-    this.state={
-        value: ''
-    }
-    this.val = this.val.bind(this)
-  }
-  val(el){
-    this.setState({
-      value: el.target.value
-    })
-  } 
-  render() {
-    return (
+export default function Weath(props){
+  let [change,setChange] = useState('')
+  return (
       <div>
-        <form className='weathForm1' onSubmit={this.props.qetWeather}>
+        <form className='weathForm1' onSubmit={props.qetWeather}>
         <div className='weathDiv1'>Узнайте погоду в вашем городе</div>
-        <input name='city' value={this.state.value} onChange={this.val} className='weathInp1' placeholder='введите в каком городе' /><br></br>
-        <button className='weathBt1'>Узнать погоду {'в '+this.state.value}</button>
+        <input name='city' className='weathInp1' onChange={(e)=>{setChange(e.target.value)}} placeholder='введите в каком городе' /><br></br>
+        <button className='weathBt1'>Узнать погоду {'в ' + change}  </button>
         </form>
       </div>
     )
-  }
 }
 
-export default Weath
